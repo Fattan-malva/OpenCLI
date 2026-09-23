f7b2f3438a8598c6e84ee5f50a9683ee563430db
Fattan-malva <156914091+Fattan-malva@users.noreply.github.com>
2026-09-24 05:29:35 +0700

    feat(db): register task capability migration

--- packages/db/src/migrations/index.ts
+++ packages/db/src/migrations/index.ts
@@ -3,5 +3,6 @@ import { migration002 } from './002-settings.js';
 import { migration003 } from './003-model-routing.js';
 import { migration004 } from './004-workflows.js';
 import { migration005 } from './005-workflow-events.js';
+import { migration006 } from './006-task-capabilities.js';
 
-export const migrations = [migration001, migration002, migration003, migration004, migration005];
+export const migrations = [migration001, migration002, migration003, migration004, migration005, migration006];
