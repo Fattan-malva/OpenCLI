f39d8a9d5c162faca7bd2cae808bef6e1ef3c6f8
Fattan-malva <156914091+Fattan-malva@users.noreply.github.com>
2026-09-24 05:14:26 +0700

    feat(db): register workflow migration

--- packages/db/src/migrations/index.ts
+++ packages/db/src/migrations/index.ts
@@ -1,5 +1,6 @@
 import { migration001 } from './001-initial.js';
 import { migration002 } from './002-settings.js';
 import { migration003 } from './003-model-routing.js';
+import { migration004 } from './004-workflows.js';
 
-export const migrations = [migration001, migration002, migration003];
+export const migrations = [migration001, migration002, migration003, migration004];
